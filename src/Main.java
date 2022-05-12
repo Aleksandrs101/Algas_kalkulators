d19dc98c173223c52dbf1a337d12f1c5577f50a8
aozolins <aleksandrs.ozolins@emergn.com>
2022-05-12 16:29:05 +0300

    pirmā versija+-

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,15 @@ public class Main {
         double atvieglojumsInv = sc.nextDouble();
         System.out.print("neapliekamais minimums ");
         double neapliekamaisMin = sc.nextDouble();
-        double neto =0;
+        double neto;
         System.out.print("nodoklu gramata ");
-        boolean ndGr = sc.nextBoolean();
-        if (NG = true) {
             if (brutoAlga <= 1667) {
                 neto = brutoAlga - (brutoAlga * 0.105) - ((1667 - (brutoAlga * 0.105) - apgSk - neapliekamaisMin - atvieglojumsInv) * 0.2 + (brutoAlga - 1667) * 0.2);
             } else {
                 neto = brutoAlga - (brutoAlga * 0.105) - ((1667 - (brutoAlga * 0.105) - apgSk - neapliekamaisMin - atvieglojumsInv) * 0.2 + (brutoAlga - 1667) * 0.23);
             }
-        }
-        else {
-            neto = brutoAlga*0.23-(brutoAlga*0.105)*0.20;
-        }
 
-            System.out.println(neto);
 
+        System.out.println(neto);
     }
 }
